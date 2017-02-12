535c2d6d2767e53a4b41ef516dffa1d2755157b3
Franck Ernewein <franck.ernewein@gmail.com>
2017-02-12 19:49:25 +0100

    test: add a demo/test page
      based on webpack
      `npm start`

--- demo/demo.js
+++ demo/demo.js
@@ -3,10 +3,14 @@ import { lightGenerator, drawLight } from '../src/index.js';
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+const {width, height} = canvas;
 
-for (let x = 1; x < 13; x++) {
-  for (let y = 1; y < 17; y++) {
-    let light = lightGenerator().create(x * 30, y * 30);
-    drawLight(ctx, light, 'rgba(255,255,255,0.5)', 'rgba(255,255,255,0.5)');
+const boxSize = 32;
+
+
+for (let x = 1; x < width / boxSize; x++) {
+  for (let y = 1; y < height / boxSize; y++) {
+    let light = lightGenerator().create(x * boxSize - boxSize / 2, y * boxSize - boxSize / 2);
+    drawLight(ctx, light, 'rgba(255,255,255,0.15)', 'rgba(255,255,255,0.7)');
   }
 }
